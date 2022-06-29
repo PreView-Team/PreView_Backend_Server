76dc8da67f0e93a3cff1aeb666b7a049ee8e55d7
tovbskvb@daum.net <tovbskvb@daum.net>
2022-06-29 19:16:52 +0900

    [add] kakao api 통신 위한 RestTemplate 빈 등록

--- src/main/java/com/preview/preview/PreviewApplication.java
+++ src/main/java/com/preview/preview/PreviewApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -13,6 +14,11 @@ public class PreviewApplication {
 	@Bean
 	public BCryptPasswordEncoder encoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 	public static void main(String[] args) {SpringApplication.run(PreviewApplication.class, args);}
